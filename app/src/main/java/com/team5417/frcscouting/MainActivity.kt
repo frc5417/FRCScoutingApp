638daabd9178ec605b20dc2b9484e3053fbf5cdc
captainbboy <bkapla01@gmail.com>
2022-07-17 16:50:35 -0500

    Starting planning out ideas for how this will work
    Want it to be configurable - Just a text file changes everything.

--- app/src/main/java/com/team5417/frcscouting/MainActivity.kt
+++ app/src/main/java/com/team5417/frcscouting/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.team5417.frcscouting
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
---
+++ app/src/main/java/com/team5417/frcscouting/ScoutingActivity.java
@@ -0,0 +1,6 @@
+package com.team5417.frcscouting;
+
+import android.app.Activity;
+
+public class ScoutingActivity extends Activity {
+}

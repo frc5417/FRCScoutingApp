9aaecacc5436452f5b1c6a8283faeec3713e32d2
captainbboy <bkapla01@gmail.com>
2022-07-17 17:42:29 -0500

    scouting activity start

--- app/src/main/java/com/team5417/frcscouting/ScoutingActivity.java
+++
@@ -1,6 +0,0 @@
-package com.team5417.frcscouting;
-
-import android.app.Activity;
-
-public class ScoutingActivity extends Activity {
-}
---
+++ app/src/main/java/com/team5417/frcscouting/ScoutingActivity.kt
@@ -0,0 +1,12 @@
+package com.team5417.frcscouting
+
+import android.content.res.Resources
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScoutingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scouting)
+    }
+}

296534ce9f0782deb2a8484211a0eba082388787
captainbboy <bkapla01@gmail.com>
2022-07-18 18:10:31 -0500

    Back btn on scouting

--- app/src/main/java/com/team5417/frcscouting/ScoutingActivity.kt
+++ app/src/main/java/com/team5417/frcscouting/ScoutingActivity.kt
@@ -1,12 +1,23 @@
 package com.team5417.frcscouting
 
+import android.content.Intent
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ScoutingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scouting)
+
+        configureBackBtn()
+    }
+
+    private fun configureBackBtn() {
+        val backBtn : Button = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener {
+            finish();
+        }
     }
 }

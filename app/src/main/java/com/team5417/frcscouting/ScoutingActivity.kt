dfe3efd4d6921755d9daa30adf6006769b3e7f84
captainbboy <bkapla01@gmail.com>
2022-10-25 22:44:27 -0500

    fixed dark theme & autohide keyboard

--- app/src/main/java/com/team5417/frcscouting/ScoutingActivity.kt
+++ app/src/main/java/com/team5417/frcscouting/ScoutingActivity.kt
@@ -3,6 +3,8 @@ package com.team5417.frcscouting
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -233,4 +235,12 @@ class ScoutingActivity : AppCompatActivity() {
             value = ""
         )
     )
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
+    }
 }
